7af4a1a5d5ffd0e15861051e4c58f4981ccc904c
Alba <adominguezpera@gmail.com>
2018-12-13 21:31:07 +0100

    About us / Contacto

---
+++ about-us.php
@@ -0,0 +1,63 @@
+<link type="text/css" rel="stylesheet" href="../css/aboutus.css">
+<link type="text/css" rel="stylesheet" href="../fpdf.css">
+
+
+<?php
+	$title = 'Inicio - weareco';
+	require_once 'head.php';
+?>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<html>
+<body>
+<h2 class="about-title">Sobre Nosotros</h2>
+<p class="about-p">Suspendisse vel turpis eu lorem porttitor luctus. Etiam metus eros, lacinia et justo id, lobortis scelerisque purus. Proin elementum sit amet 
+    tortor sed elementum. Vestibulum sollicitudin lacus vitae orci vehicula dignissim. In sit amet lacus in libero placerat hendrerit. Nam scelerisque porta quam,
+    non accumsan ex ultricies sit amet. Integer pharetra faucibus nibh quis tincidunt. Morbi quam erat, mollis a imperdiet eu, condimentum nec nisi.
+    Curabitur non est dapibus, malesuada mi at, bibendum est. In pellentesque condimentum dui nec maximus. Donec scelerisque felis eget risus placerat,
+    sed feugiat nisl gravida.</p>
+<br>
+<div class="team">
+<div style="text-align:center;" class="prorow">
+  <div class="column">
+    <div class="card">
+      <img src="./img/team1.jpg" alt="Jane" style="width:100%">
+      <div class="container">
+        <h2 class="about-name">Jane Doe</h2>
+        <p class="title">CEO & Founder</p>
+        <p class="p-card" >Fusce eu eros ut justo volutpat suscipit. Duis eget suscipit est, ac euismod nisi. </p>
+        <p class="p-card">example@example.com</p>
+        <p><a href="mailto:webmaster@example.com"><button class="button">Contacta</button></p></a>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="./img/team2.jpg" alt="Mike" style="width:100%">
+      <div class="container">
+        <h2 class="about-name">Mike Ross</h2>
+        <p class="title">Director de arte</p>
+        <p class="p-card">Fusce eu eros ut justo volutpat suscipit. Duis eget suscipit est, ac euismod nisi. </p>
+        <p class="p-card">example@example.com</p>
+        <p><a href="mailto:webmaster@example.com"><button class="button">Contacta</button></p></a>
+      </div>
+    </div>
+  </div>
+  <div class="column">
+    <div class="card">
+      <img src="./img/team3.jpg" alt="John" style="width:100%">
+      <div class="container">
+        <h2 class="about-name">John Doe</h2>
+        <p class="title">Diseñador</p>
+        <p class="p-card">Fusce eu eros ut justo volutpat suscipit. Duis eget suscipit est, ac euismod nisi. </p>
+        <p class="p-card">example@example.com</p>
+        <p><a href="mailto:webmaster@example.com"><button class="button">Contacta</button></p></a>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</body>
+</html>
+
+
---
+++ contact.php
@@ -0,0 +1,36 @@
+<link type="text/css" rel="stylesheet" href="../fpdf.css">
+
+
+<?php
+	$title = 'Inicio - weareco';
+	require_once 'head.php';
+?>
+
+
+
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<h2 class="about-title">Contacta con nosotros</h2>
+<p class="about-p">Suspendisse vel turpis eu lorem porttitor luctus. Etiam metus eros, lacinia et justo id, lobortis scelerisque purus. Proin elementum sit amet 
+    tortor sed elementum. Vestibulum sollicitudin lacus vitae orci vehicula dignissim. In sit amet lacus in libero placerat hendrerit. Nam scelerisque porta quam,
+    non accumsan ex ultricies sit amet.</p>
+<div class="contacto">
+<div class="container">
+  <form action="/action_page.php">
+    <label for="fname">Nombre</label>
+    <input type="text" id="fname" name="firstname" placeholder="Escribre aquí tu nombre..">
+    <label for="lname">Appellidos</label>
+    <input type="text" id="lname" name="lastname" placeholder="Escribre aquí tus apellidos..">
+    <label for="subject">Mensaje</label>
+    <textarea id="subject" name="subject" placeholder="Escribe aquí tu mensaje.." style="height:200px"></textarea>
+
+    <input type="submit" value="Enviar">
+  </form>
+</div>
+</div>
+
+</body>
+</html>
